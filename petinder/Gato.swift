b6fcaa8cf37e048e6fbecd1bb334c020f0a44df8
Eduarda Linhares Mello <duda.mello1@hotmail.com>
2019-05-03 10:20:45 -0300

    classe cachorro pronta

---
+++ petinder/Gato.swift
@@ -0,0 +1,25 @@
+//
+//  Gato.swift
+//  petinder
+//
+//  Created by Eduarda Mello on 03/05/19.
+//  Copyright © 2019 Eduarda Mello. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Gato{
+    var image: UIImage
+    var name: String
+    var description: String
+    var age: String
+    
+    init(image: UIImage, name: String, description: String, age: String) {
+        self.name = name
+        self.image = image
+        self.description = description
+        self.age = age
+    }
+
+}
---
+++ petinder/GatoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  GatoViewController.swift
+//  petinder
+//
+//  Created by Eduarda Mello on 03/05/19.
+//  Copyright © 2019 Eduarda Mello. All rights reserved.
+//
+
+import UIKit
+
+class GatoViewController: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
